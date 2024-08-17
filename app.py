c94557039171dbdb8b889572772f6f773d896ed5
abdoimane203 <160982204+abdoimane203@users.noreply.github.com>
2024-08-17 15:34:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import torch
 from diffusers import FluxPipeline
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, render_template
 from io import BytesIO
 
 app = Flask(__name__)
@@ -9,22 +9,26 @@ app = Flask(__name__)
 pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
 pipe.enable_model_cpu_offload()
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/generate-image', methods=['POST'])
 def generate_image():
     data = request.json
     prompt = data.get('prompt', '')
     
     if not prompt:
-        return jsonify({'error': 'No prompt provided'}), 400
+        return jsonify({'error': 'لم يتم توفير نص وصفي'}), 400
     
     try:
         # توليد الصورة
         image = pipe(
             prompt,
-            height=512,  # تقليل الحجم لتوفير الموارد
+            height=512,
             width=512,
             guidance_scale=3.5,
-            num_inference_steps=30,  # تقليل عدد الخطوات لتسريع العملية
+            num_inference_steps=30,
             max_sequence_length=512,
             generator=torch.Generator("cpu").manual_seed(0)
         ).images[0]
